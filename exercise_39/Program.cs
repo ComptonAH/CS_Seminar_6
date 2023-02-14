7092ea34472f18a527f7c0804017123eccd671c1
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-14 12:03:18 +0300

    exercsise 39 had been accomplished

---
+++ exercise_39/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] RandomArray()
+{
+    int[] Array = new int[new Random().Next(3,11)];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(11);
+        Console.Write($"{Array[i]} ");
+    }
+    Console.WriteLine();
+    return Array;
+}
+
+int[] ReverseArray(int[] Array)
+{
+    int[] RevArray = new int[Array.Length];
+    for (int j = 0, i = Array.Length-1; j < Array.Length; j++,i--)
+    {
+        RevArray[j] = Array[i];
+        Console.Write($"{RevArray[j]} ");   
+    }
+    return Array;
+}
+
+ReverseArray(RandomArray());
