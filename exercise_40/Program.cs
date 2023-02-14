bb3459bbd6ad99b242a90667292b904616883428
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-14 12:11:37 +0300

    exercsise 40 had been accomplished

---
+++ exercise_40/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter the first value of the side of the triangle: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second value of the side of the triangle: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the third value of the side of the triangle: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b+c)
+{
+    if (b < a+c)
+    {
+        if (c < a+b)
+        {
+            Console.WriteLine("This triangle can be exist");
+        }
+        else Console.WriteLine("This triangle can NOT be exist");
+    }
+    else Console.WriteLine("This triangle can NOT be exist");
+}
+else Console.WriteLine("This triangle can NOT be exist");
