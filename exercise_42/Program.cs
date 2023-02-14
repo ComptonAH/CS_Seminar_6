8cc0a83a2e1d34fb9af083214ee84ac4b5909b06
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-14 13:39:53 +0300

    exercsise 42 had been accomplished

---
+++ exercise_42/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a number in decimal numeral system : ");
+int Decimal = Convert.ToInt32(Console.ReadLine());
+string s = "";
+while (Decimal > 0)
+{
+    s = Decimal%2 + s;
+    Decimal /= 2;
+}
+Console.Write(s);
